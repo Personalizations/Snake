5b700996324b738c885eb4eff89227ef82da141b
TeddyMcadams <guny7118@gmail.com>
2025-08-17 09:45:33 +0800

    renew: add a start game button

---
+++ function/start_game.py
@@ -0,0 +1,9 @@
+# function/start_game.py
+from main import main as start_snake_game
+
+def start_game():
+    print("Strat Game...")
+    start_snake_game()
+
+if __name__ == "__main__":
+    start_game()
